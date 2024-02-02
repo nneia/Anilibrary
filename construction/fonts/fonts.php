b4b80cde7d8f7512f4c8c94b395ea4d083b5408a
Bazhirov Bizhan <94357567+nneia@users.noreply.github.com>
2024-02-03 04:37:17 +0900

    Add files via upload

---
+++ construction/fonts/fonts.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="../construction/fonts/WeblySleek_UI_Semibold/stylesheet.css">
+<link rel="stylesheet" href="../construction/fonts/Clear sans/clearsans_bold_macroman/stylesheet.css">
+<link rel="stylesheet" href="../construction/fonts/Clear sans/clearsans_medium_macroman/stylesheet.css">
+<link rel="stylesheet" href="../construction/fonts/webfonts_PWMarker/stylesheet.css">
---
+++ construction/footer/footer.php
@@ -0,0 +1,21 @@
+<footer class="footer-container">
+	<div class="footer-left">
+		<img class="logo-footer" src="../construction/icons/anilibrary-logo.svg" alt="">
+		<p class="footer-slogan">This website has been created in order to help people 
+easily find anime that they will most likely enjoy. After watching you can discuss them in the community and find likeminded people and friends.
+</p>
+	</div>
+	<div class="footer-right">
+		<div class="footer-nav">
+			<p class="footer-nav-text">About us</p>
+			<p class="footer-nav-text">Contact us</p>
+			<p class="footer-nav-text">Terms and Privacy</p>
+		</div>
+		<div class="footer-reflection">
+			<p class="reflection-text">Report a bug</p>
+			<p class="spacing-pillar">|</p>
+			<p class="reflection-text">Community voice</p>
+		</div>
+	</div>
+	<div class="footer-bottom">@Anilibrary 2020</div>
+</footer>
